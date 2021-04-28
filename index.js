d613a7009f20d2f0b4d52809dae40aba08f4f19b
anseal <bottomless.box@gmail.com>
2021-04-28 18:51:41 +0800

    FIX: export

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { HarLogger } from './src/har_logger.js'
 import { ProfilersCombo } from './src/profilers_combo.js'
 import { merge_cpuprofile_and_har } from './src/merge_cpuprofile_and_har.js'
 
-export default {
+export {
 	HarLogger,
 	ProfilersCombo,
 	merge_cpuprofile_and_har,
